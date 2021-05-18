179b4257004433f37e15332a924a6e80d8e09901
Ilya Zorin <ilya-zorin@yandex.ru>
2021-05-18 18:35:24 +0700

    add http_client, init requests

--- calculator/ui/src/app/app.component.ts
+++ calculator/ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  constructor(
+    private appService: AppService
+  ) { }
   title = 'ui';
+
+  calculate() {
+    console.log('calculate')
+    this.appService.get('http://0.0.0.0:9293/calculate')
+      .subscribe((data: any) => {
+        console.log('data', data)
+      },
+      error => {
+        console.log(error)
+      });
+  }
 }
--- calculator/ui/src/app/app.module.ts
+++ calculator/ui/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppService } from './app.service'
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
